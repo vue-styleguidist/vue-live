6d875b23c090b939d08c2b76554b9da3a1560661
elevatebart <ledouxb@me.com>
2019-04-27 23:19:37 -0500

    extenalize lib in prod

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,15 @@ module.exports = {
       .add("./demo/main.js");
 
     config.resolve.alias.set("vue$", "vue/dist/vue.esm.js");
-    if (false) {
+    if (process.env.NODE_ENV === "production") {
       config.externals({
-        "codemirror/lib/codemirror.css": "codemirror/lib/codemirror.css",
-        "codemirror/mode/vue/vue.js": "codemirror/mode/vue/vue.js",
-        "vue-codemirror": "vue-codemirror",
         acorn: "acorn",
         buble: "buble",
         walkes: "walkes",
         "hash-sum": "hash-sum",
         "vue-template-compiler": "vue-template-compiler",
-        "rewrite-imports": "rewrite-imports"
+        "rewrite-imports": "rewrite-imports",
+        prismjs: "prismjs"
       });
     }
   }
