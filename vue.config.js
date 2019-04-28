c270b9dd82bd9de1b9c9bf362696a5ebb849fb21
elevatebart <ledouxb@me.com>
2019-04-27 22:27:35 -0500

    setup externals for lib

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,17 @@ module.exports = {
       .add("./demo/main.js");
 
     config.resolve.alias.set("vue$", "vue/dist/vue.esm.js");
+
+    config.externals({
+      "codemirror/lib/codemirror.css": "codemirror/lib/codemirror.css",
+      "codemirror/mode/vue/vue.js": "codemirror/mode/vue/vue.js",
+      "vue-codemirror": "vue-codemirror",
+      acorn: "acorn",
+      buble: "buble",
+      walkes: "walkes",
+      "hash-sum": "hash-sum",
+      "vue-template-compiler": "vue-template-compiler",
+      "rewrite-imports": "rewrite-imports"
+    });
   }
 };
