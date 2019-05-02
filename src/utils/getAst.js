dbc1dc55c79d5888781b1c62b2aec5ebac3a70e3
elevatebart <ledouxb@me.com>
2019-05-02 06:48:36 -0500

    style: prettify getast

--- src/utils/getAst.js
+++ src/utils/getAst.js
@@ -1,8 +1,8 @@
-import { parse } from 'acorn'
+import { parse } from "acorn";
 
 export default function getAst(code) {
-	return parse(code, {
-		ecmaVersion: 2019,
-		sourceType: 'module'
-	})
+  return parse(code, {
+    ecmaVersion: 2019,
+    sourceType: "module"
+  });
 }
