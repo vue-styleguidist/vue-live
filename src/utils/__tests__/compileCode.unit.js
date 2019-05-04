38118443cba9f66aeaaf55685dc80172aac5fb57
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-05-04 18:56:35 -0500

    test: add some missing unit tests

--- src/utils/__tests__/compileCode.unit.js
+++ src/utils/__tests__/compileCode.unit.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-eval */
-import compileCode from '../compileCode'
+import compileCode from "../compileCode";
 
-describe('compileCode', () => {
+describe("compileCode", () => {
   // eslint-disable-next-line no-unused-vars
-  let dummySet
+  let dummySet;
   // eslint-disable-next-line no-unused-vars
   function Vue(param) {
-    dummySet = param
+    dummySet = param;
   }
-  it('bake template into a new Vue', () => {
+  it("bake template into a new Vue", () => {
     const sut = compileCode(`
 <template>
 	<div/>
@@ -18,28 +18,28 @@ const param = 'Foo'
 export default {
 	param
 }
-</script>`)
-    eval(sut.script)
-    expect(dummySet).toMatchObject({ param: 'Foo' })
-  })
+</script>`);
+    eval(sut.script);
+    expect(dummySet).toMatchObject({ param: "Foo" });
+  });
 
-  it('shoud be fine with using the `new Vue` structure', () => {
+  it("shoud be fine with using the `new Vue` structure", () => {
     const sut = compileCode(`
 let param = 'Bar';
 new Vue({
 	param
-});`)
-    eval(sut.script)
-    expect(dummySet).toMatchObject({ param: 'Bar' })
-  })
+});`);
+    eval(sut.script);
+    expect(dummySet).toMatchObject({ param: "Bar" });
+  });
 
-  it('shoud work with the vsg way', () => {
+  it("shoud work with the vsg way", () => {
     const sut = compileCode(`
 		let param = 'BazBaz';
 		<div>
 			<button> {{param}} </button>
 		</div>
-		`)
-    expect(sut.script.trim()).toBe("let param = 'BazBaz';")
-  })
-})
+		`);
+    expect(sut.script.trim()).toBe("var param = 'BazBaz';");
+  });
+});
