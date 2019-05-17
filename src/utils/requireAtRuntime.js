74b32e283237a2fdeaf8d12cae21d64bc71da657
elevatebart <ledouxb@me.com>
2019-05-17 13:39:11 -0500

    fix: right-hand side of 'in' when no require

--- src/utils/requireAtRuntime.js
+++ src/utils/requireAtRuntime.js
@@ -2,6 +2,7 @@
  * Return module from a given map (like {app: require('app')}) or throw.
  */
 export default function requireAtRuntime(requires, filepath) {
+  requires = requires || {};
   if (!(filepath in requires)) {
     throw new Error(
       "import or require() statements can be added only by setting it using the requires prop"
