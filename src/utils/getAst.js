1f6d53c8a404bf071968576f7da99be2e192c1fd
elevatebart <ledouxb@me.com>
2019-05-16 19:00:47 -0500

    tests: fix tests

--- src/utils/getAst.js
+++ src/utils/getAst.js
@@ -1,8 +1,8 @@
-var acorn = require("acorn");
-var jsx = require("acorn-jsx");
+import { Parser } from "acorn";
+const jsx = require("acorn-jsx");
 
 export default function getAst(code) {
-  return acorn.Parser.extend(jsx()).parse(code, {
+  return Parser.extend(jsx()).parse(code, {
     ecmaVersion: 2019,
     sourceType: "module"
   });
