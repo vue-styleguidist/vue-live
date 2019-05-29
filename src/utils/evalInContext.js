83740d811bf34636346d4254c1d409252abb0883
elevatebart <ledouxb@me.com>
2019-05-29 16:32:49 -0500

    docs: add types to eval in context params

--- src/utils/evalInContext.js
+++ src/utils/evalInContext.js
@@ -1,8 +1,8 @@
 /**
  * evaluate es5 code in the browser
  * and return value if there s a return statement
- * @param {*} code the body of the funtion to exectute
- * @param {*} require the fake function require
+ * @param {String} code the body of the funtion to execute
+ * @param {Function} require the fake function require
  */
 export default function evalInContext(code, require) {
   // eslint-disable-next-line no-new-func
