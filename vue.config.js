c7cc471d00d6e80206c2ea2c15acf792679734c2
Barthélémy Ledoux <bledoux@shipbob.com>
2020-03-24 09:28:26 -0500

    progress on IE11 compatibility

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,16 @@ module.exports = {
       .add("./demo/main.js");
 
     config.resolve.alias.set("vue$", "vue/dist/vue.esm.js");
-  }
+  },
+  transpileDependencies: [
+    "regexpu-core",
+    "strip-ansi",
+    "ansi-regex",
+    "ansi-styles",
+    "unicode-match-property-ecmascript",
+    "unicode-match-property-value-ecmascript",
+    "acorn-jsx",
+    "camelcase",
+    "regexpp"
+  ]
 };
