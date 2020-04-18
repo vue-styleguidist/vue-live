ac53405a1b01ff92596417ee1ef159fdc20ec6e0
elevatebart <ledouxb@me.com>
2020-04-18 15:44:46 -0500

    test: make unit tests pass

--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,17 @@ module.exports = {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest"
+    "^.+\\.jsx?$": "babel-jest",
   },
   transformIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   snapshotSerializers: ["jest-serializer-vue"],
-  testMatch: [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
-  ],
+  testMatch: ["**/__tests__/*.(js|jsx|ts|tsx)"],
   testURL: "http://localhost/",
   watchPlugins: [
     "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
-  ]
+    "jest-watch-typeahead/testname",
+  ],
 };
