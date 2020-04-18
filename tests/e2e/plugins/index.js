6ca7e29dc58e0a1d967aadf9a83c4ffa48ad9ad3
elevatebart <ledouxb@me.com>
2020-04-18 15:39:15 -0500

    test: fix linting problem

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -5,7 +5,7 @@
 // as explained in the cypress docs
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
 
-/* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
+// /* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
 // const webpack = require('@cypress/webpack-preprocessor')
 
 module.exports = (on, config) => {
@@ -15,10 +15,10 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
-    fixturesFolder: 'tests/e2e/fixtures',
-    integrationFolder: 'tests/e2e/specs',
-    screenshotsFolder: 'tests/e2e/screenshots',
-    videosFolder: 'tests/e2e/videos',
-    supportFile: 'tests/e2e/support/index.js'
-  })
-}
+    fixturesFolder: "tests/e2e/fixtures",
+    integrationFolder: "tests/e2e/specs",
+    screenshotsFolder: "tests/e2e/screenshots",
+    videosFolder: "tests/e2e/videos",
+    supportFile: "tests/e2e/support/index.js",
+  });
+};
