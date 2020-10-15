3e53baaa8ab680e9fa36a3015e05483dc7852893
ElevateBart <ledouxb@gmail.com>
2020-10-15 09:48:09 -0500

    fix: make is installed only once

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import VueLive from "./VueLive.vue";
 import VueLivePreview from "./Preview.vue";
-import VueLiveEditor from './Editor.vue';
+import VueLiveEditor from "./Editor.vue";
 
 // Export components individually
 export { VueLive };
@@ -8,11 +8,29 @@ export { VueLivePreview };
 export { VueLiveEditor };
 
 // What should happen if the user installs the library as a plugin
-function install(Vue) {
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
   Vue.component("VueLive", VueLive);
   Vue.component("VueLivePreview", VueLivePreview);
   Vue.component("VueLiveEditor", VueLiveEditor);
 }
 
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
 // Export the library as a plugin
-export default { install: install };
+export default plugin;
