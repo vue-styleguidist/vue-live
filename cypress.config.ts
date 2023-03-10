41130e205f77c2bc2a5102d093d2b45476a90c1a
elevatebart <bart@cypress.io>
2023-03-10 13:37:12 -0600

    add cypress cloud integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'wbesaj',
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
